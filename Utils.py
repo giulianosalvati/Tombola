717461680620268f9db904abada3848138fcd6cb
MariaElenaContini <con.elena@hotmail.it>
2022-02-04 17:04:22 +0100

    Aggiunto commenti nel file Utils.py

--- Utils.py
+++ Utils.py
@@ -26,13 +26,32 @@ def initialize_parser():
     return parser.parse_args()
 
 def check_numero_giocatori(n_giocatori):
-    if n_giocatori>1 and n_giocatori<13:
+    """
+    verifica se il numero di giocatori è rispetta le regole (>1 e <=8)
+    restituisce il numero di giocatori se le regole sono rispettate,
+    altrimenti stampa un messaggio e termina il programma 
+    
+    parametri di ingresso:
+        n_giocatori (int): numero dei giocatori
+    """
+    if n_giocatori>1 and n_giocatori<9:
         return n_giocatori
     else:
         print('Errore - Il numero di giocatori deve essere superiore ad 1 fino ad un massimo di 12')
         exit()
 
 def check_lista_cartelle(n_giocatori, lista_cartelle):
+    """
+    verifica se la linghezza della lista di cartelle inserita corrisponde al numero
+    di giocatori 
+    restituisce la lista delle cartelle se coincidono e se ad ogni giocatore sono assegnate
+    da 1 a 5 cartelle,
+    altrimenti stampa un messaggio e termina il programma 
+    
+    parametri di ingresso:
+        n_giocatori (int): numero dei giocatori
+        lista_cartelle (int[]): lista delle cartelle da assegnare a ciascun giocatore
+    """
     if len(lista_cartelle)!=n_giocatori:
         print('Errore - Vi sono giocatori a cui non è stata assegnata alcuna cartella')
         if len(lista_cartelle)<n_giocatori:
@@ -61,7 +80,18 @@ def check_lista_cartelle(n_giocatori, lista_cartelle):
 
 
 
-def assegnazione_cartelle(n_giocatori,lista_cartelle,gruppi_cartelle): 
+def assegnazione_cartelle(n_giocatori,lista_cartelle,gruppi_cartelle):
+    """
+    Assegna a ciascun giocatore il numero richiesto di cartelle facendo in modo che
+    non sia possibile assegnare a più giocatori la medesima cartella.
+    Restituisce i giocatori inizializzati ognuno con il proprio nome, numero di cartelle che ha e 
+    quel numero di cartelle.
+    
+    parametri di ingresso:
+        n_giocatori (int): numero dei giocatori
+        lista_cartelle (int[]): lista delle cartelle da assegnare a ciascun giocatore
+        gruppi_cartelle: cartelle generate da assegnare a ciascun giocatore
+    """
     
     giocatori={}
     nomi=['cartella1','cartella2','cartella3','cartella4','cartella5','cartella6']
