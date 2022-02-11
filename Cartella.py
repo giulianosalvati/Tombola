47388ff24430a225683d672e13f068344a178698
MariaElenaContini <con.elena@hotmail.it>
2022-02-11 13:25:10 +0100

    Aggiunto Commenti Cartella.py

--- Cartella.py
+++ Cartella.py
@@ -10,63 +10,86 @@ import numpy as np
 class Cartella:
     
     def __init__(self,cartella=None,conta_colonne=None,conta_righe=None):
-        self.cartella= np.zeros((3,9))
-        # Inizializzo 2 matrici, conta_colonne (6,9) e conta_righe(6,3), che conteranno il numero di caselle occupate in ogni cartella, da utilizzare per verificare che i vincoli sulle righe(max 5 numeri) e sulle colonne siano rispettati
-        # Nelle 2 matrici le righe i rappresentano le cartelle a cui si fa riferimento mentre le colonne rappresentano rispettivamente:
-        #in conta_colonne(i,j): la colonna j-esima della cartella i-esima (j=0,1,2..9)
-        #in conta_righe(i,r): la riga r-esima di ciascuna cartella i (r=0,1,2)
-        self.conta_colonne= np.zeros(9)
-        self.conta_righe= np.zeros(3)
+        self.cartella = np.zeros((3,9))
+        self.conta_colonne = np.zeros(9) # Contatore degli elementi sulle colonne della cartella
+        self.conta_righe = np.zeros(3) # Contatore degli elementi sulle righe della cartella
     
-    def aggiorna_conteggio(self,index_riga, index_colonna):
+    def aggiorna_conteggio(self,index_riga, index_colonna): 
+        """
+        Metodo che aggiorna i contatori su righe e colonne, incrementandoli di 1
         
-        self.conta_colonne[index_colonna]+=1
-        self.conta_righe[index_riga] +=1
+        Input
+        -----
+        index_riga (int): indice riga da aggiornare
+        index_colonna (int ): indice colonna da aggiornare
+        
+        Output
+        ------
+        Nan
+        
+        """
+        
+        self.conta_colonne[index_colonna] += 1
+        self.conta_righe[index_riga] += 1
     
     
     def inserisci_numero(self,index_riga,index_colonna,numero):
         
         """
-        Metodo che permette di inserire il numero in ingresso nella posizione [riga colonna] della cartella di indice dato
+        Metodo che permette di inserire il numero in ingresso nella posizione [index_riga, index_colonna] 
+        della cartella ed aggiorna i contatori su righe e colonne con il metodo della classe
+        'aggiorna_conteggio'
         
         Input
-        -------
-        index_cartella (int) : indice della cartella del gruppo
-        riga (int): riga in cui inserire il numero
-        colonna (int ): colonna in cui inserire il numero
+        -----
+        index_riga (int): indice riga in cui inserire il numero
+        index_colonna (int ): indice colonna in cui inserire il numero
         numero (int): il numero da inserire nella cartella
         
-        Returns
-        -------
+        Output
+        ------
         Nan
         
         """
-        self.cartella[index_riga,index_colonna]= numero
+        self.cartella[index_riga,index_colonna] = numero
         self.aggiorna_conteggio(index_riga, index_colonna)
     
     def elimina_numero(self,index_riga,index_colonna):
         
         """
-        Tolgo dalla cartella il valore nella posizione [riga ,colonna]
+        Tolgo dalla cartella il valore nella posizione [index_riga ,index_colonna] ponendolo
+        a zero ed aggiorno i contatori su righe e colonne della cartella 
+        con un decremento unitario
     
         Input
-        -------
-        index_cartella (int) : indice della cartella del gruppo
-        riga (int): riga in cui inserire il numero
-        colonna (int ): colonna in cui inserire il numero
+        -----
+        index_riga (int): indice riga in cui inserire il numero
+        index_colonna (int ): indice colonna in cui inserire il numero
         
-        Returns
-        -------
+        Output
+        ------
         Nan
         
         """
-        self.cartella[index_riga,index_colonna]=0
-        self.conta_colonne[index_colonna]-=1
-        self.conta_righe[index_riga] -=1
+        self.cartella[index_riga,index_colonna] = 0
+        self.conta_colonne[index_colonna]-= 1
+        self.conta_righe[index_riga] -= 1
     
     def azzera_contatori(self):
-        self.conta_colonne= np.zeros(9)
-        self.conta_righe= np.zeros(3)
+        """
+        Metodo che azzera i contatori su righe e colonne della cartella
+        
+        Input
+        -----
+        NaN
+        
+        Output
+        ------
+        NaN
+        
+        """
+        self.conta_colonne = np.zeros(9)
+        self.conta_righe = np.zeros(3)
         
         
     
